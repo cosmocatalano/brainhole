2ec4d655984126a0724efd71279e97f578d9f15c
Cosmo Catalano <cosmo@cosmocatalano.com>
2019-06-22 19:59:42 -0600

    modified for Cloudinary URLs
    still hacky (explode instead of parsing) but it works

--- brainhole.php
+++ brainhole.php
@@ -2,32 +2,20 @@
 	
 //control variables for your embedding pleasure
 $stories = 3; //number of stories
-$image_dimension = '1x1'; //dimensions of image, string only (bummer); '16x9' also works
-$image_width = 100; //image width in pixels
+$image_width = 'w_100'; //image width in pixels
+$image_height = 'h_100'; //image height in pixels
+$image_contain = 'c_fill'; //other options at https://cloudinary.com/documentation/image_transformations#resizing_and_cropping_images
+$image_quality = 'q_64'; //see above
+$image_prog = 'fl_progressive';
 
-//breaks image url out of partial Clickhole page 
-function get_og($html) {
-	$og_explode = explode('"og:image" content="', $html);
-	$og_url = explode('"/>', $og_explode[1]);
-	$og_image_url = $og_url[0];
-	return $og_image_url;
-}
 
-//changes og:image to your specified dimensions and size
-function set_image($img_url, $image_dimension, $image_width) {
-	$url_shards = explode('/', (string)$img_url);
-	$url_shards[6] = $image_dimension;
-	$url_shards[7] = $image_width;
-	$new_url = 'http://'.$url_shards[2].'/'. $url_shards[3].'/'.$url_shards[4].'/'.$url_shards[5].'/'.$url_shards[6].'/'.$url_shards[7].'.jpg';
-	return $new_url;
-}
-//Grabbing part of the target page to get the og:image URL; choose 2048 bytes arbitrarily,
-//getting the og:image url, changing it to our desired size
-function get_image($link, $image_dimension, $image_width) {
-	$target_page = file_get_contents($link, NULL, NULL, 0, 2000);
-	$image_url = get_og($target_page);
-	$new_image = set_image($image_url, $image_dimension, $image_width);
-	return $new_image;
+//get cloudinary image of desired size
+function hack_image($desc_array, $img_w, $img_h, $img_c, $img_q, $img_p) {
+	$img_shard = explode('img src="', $desc_array[0]);
+	$img_url = explode('" />',$img_shard[1]);
+	$img_slug = explode('c_fit', $img_url[0]);
+	$img_id = explode('w_636', $img_url[0]);
+	return $img_slug[0].$img_c.','.$img_p.','.$img_w.','.$img_h.$img_id[1];
 }
 
 //replacing the Google Analytics info so they know where it came from
@@ -44,7 +32,7 @@ $selected = array_slice($story_index, 0, $stories);
 
 //converting the RSS feed to an array -- http://stackoverflow.com/questions/2454979/get-rss-feed-into-php-array-possible/2455028#2455028
 $feed = 'http://www.clickhole.com/feeds/rss';
-$feed_to_array = (array) simplexml_load_file($feed);
+$feed_to_array = (array) simplexml_load_file($feed, 'SimpleXMLElement', LIBXML_NOCDATA);
 
 //selecting the data for the three random articles
 $articles = array();
@@ -52,11 +40,12 @@ foreach($selected as $selector) {
 	array_push($articles, $feed_to_array[channel]->item[$selector]);
 }
 
+
 //turn that crank
 //this currently generates unstyled HTML. You might want to do something different with it.
 foreach ($articles as $article) {
+	$image = hack_image($article->description, $image_width, $image_height, $image_contain, $image_quality, $image_prog);
 	$link = retag_link($article->link);
-	$image = get_image($link, $image_dimension, $image_width);
 	$title = $article->title;
 	$linked_image = '<a href="'.$link.'"><img src="'.$image.'"></a>';
 	$linked_title = '<a href="'.$link.'"><span>'.mb_convert_encoding($title, "HTML-ENTITIES", "UTF-8").'</span></a>';
